7ec6679632f3753f03eb285f1bfb9eedfd2fdd4b
BCAvarshini <varshinijakkali@gmail.com>
2025-04-29 08:57:21 +0530

    description

--- Employee.java
+++ Employee.java
@@ -5,12 +5,14 @@ public class Employee {
     String name;
     int id;
     double salary;
+    String location;
 
     //Constructor
     public Employee(String name, int id, double salary) {
         this.name = name;
         this.id = id;
         this.salary = salary;
+        this.location = "India"; // Default location
     }
 
     //Method to display employee details
@@ -18,13 +20,16 @@ public class Employee {
         System.out.println("Employee Name: " + name);
         System.out.println("Employee ID: " + id);
         System.out.println("Salary: ₹" + salary);
+        System.out.println("Location: " + location);
     }
 
     //Main method
     public static void main(String[] args) {
         //Creating an object of Employee class
         Employee emp1 = new Employee("Varshini", 101, 50000.0);
+
         //Calling method to display employee details
         emp1.displayInfo();
+
     }
 }
